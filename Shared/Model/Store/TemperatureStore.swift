5a04683bd71e3c521baaea2ca6d541c270dd6c36
Shun Yamazaki <syunn820@gmail.com>
2021-11-01 21:54:43 +0900

    使ってないクラス削除

--- Shared/Model/Store/TemperatureStore.swift
+++ Shared/Model/Store/TemperatureStore.swift
@@ -19,3 +19,24 @@ class TemperatureStore: ObservableObject{
         self.value = closure(self.value)
     }
 }
+
+final class Store<T> : ObservableObject{
+    @Published private var value:T
+    
+    init(_ initialValue:T){
+        self.value = initialValue
+    }
+    
+    func update(_ closure:(T) -> T) -> Void{
+        self.value = closure(self.value)
+    }
+    
+    func onReceive(_ receivedValue:@escaping(T) -> Void) -> AnyCancellable{
+        return self.$value.sink(receiveValue: receivedValue)
+    }
+}
+
+protocol TemperatureStoreDelegate{
+    func onReceive(_ receiveValue:@escaping(Temperature) -> Void) -> AnyCancellable
+    func update(_ closure:(Temperature) -> Temperature) -> Void
+}
